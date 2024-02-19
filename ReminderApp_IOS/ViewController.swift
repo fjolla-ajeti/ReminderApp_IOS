0e1c9dfaf44f1a713dee8a5b6daa41823759fd82
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-02-19 10:00:59 -0800

    Enhanced table view functionality by implementing selection handling and data population

--- ReminderApp_IOS/ViewController.swift
+++ ReminderApp_IOS/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var table: UITableView!
+    var models = [MyReminder]()
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
@@ -18,10 +19,26 @@ class ViewController: UIViewController {
 
 }
 extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     
 }
 extension ViewController:UITableViewDataSource{
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+
+        return cell
+    }
 }
 struct My Reminder{
     let title: String
